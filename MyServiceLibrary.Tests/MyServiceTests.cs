ea365a80da4db554df6215b3be3a12884666d44a
VladislavStarovoitov <vladislav.starovoitov94@gmail.com>
2016-11-01 15:24:45 +0300

    Add SampleService

---
+++ MyServiceLibrary.Tests/MyServiceTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyServiceLibrary.Tests
+{
+    [TestClass]
+    public class MyServiceTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Add_NullUser_ExceptionThrown()
+        {
+            var service = new MyService();
+
+            service.Add(null);
+        }
+
+        [TestMethod]
+        public void Add_ValidUser_ValidUserIsAdded()
+        {
+            var service = new MyService();
+
+            int id = service.Add(new User
+            {
+                FirstName = "Vlad",
+                LastName = "Star",
+                DateOfBirth = DateTime.Now
+            });
+            Assert.AreEqual(0, id);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidUserException))]
+        public void Add_FirstNameIsNull_ExceptionThrown()
+        {
+            var service = new MyService();
+
+            service.Add(new User
+            {
+                FirstName = null,
+                LastName = "Star",
+                DateOfBirth = DateTime.Now
+            });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidUserException))]
+        public void Add_LastNameIsNull_ExceptionThrown()
+        {
+            var service = new MyService();
+
+            service.Add(new User
+            {
+                FirstName = "Vlad",
+                LastName = null,
+                DateOfBirth = DateTime.Now
+            });
+        }
+
+        public void Find_UserId_ReturnUser()
+        {
+
+        }
+    }
+}
---
+++ MyServiceLibrary/InvalidUserException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace MyServiceLibrary
+{
+    [Serializable]
+    public class InvalidUserException : Exception
+    {
+        public InvalidUserException()
+        {
+        }
+
+        public InvalidUserException(string message) : base(message)
+        {
+        }
+
+        public InvalidUserException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected InvalidUserException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
